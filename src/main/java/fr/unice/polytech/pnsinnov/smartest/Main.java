243a712eccbe942600d8b446f35b8a64e2ec9998
FlorianLehmann <Lehmann.florian@yandex.com>
2018-05-28 14:08:26 +0200

    add dependencies creation of the file tree

---
+++ src/main/java/fr/unice/polytech/pnsinnov/smartest/Main.java
@@ -0,0 +1,5 @@
+package fr.unice.polytech.pnsinnov.smartest;
+
+public class Main {
+
+}
---
+++ src/test/java/fr/unice/polytech/pnsinnov/smartest/MainTest.java
@@ -0,0 +1,7 @@
+package fr.unice.polytech.pnsinnov.smartest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
