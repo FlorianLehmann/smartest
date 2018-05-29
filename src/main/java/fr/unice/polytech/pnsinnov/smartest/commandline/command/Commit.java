aad3fcecd9216c8a8688f9106e5a634e9ec0239c
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-05-29 17:00:58 +0200

    A message is now required in the commandline. PNSE-6

--- src/main/java/fr/unice/polytech/pnsinnov/smartest/commandline/command/Commit.java
+++ src/main/java/fr/unice/polytech/pnsinnov/smartest/commandline/command/Commit.java
@@ -5,6 +5,10 @@ import picocli.CommandLine;
 
 @CommandLine.Command(name = "commit", description = "Run tests then record changes to the repository.")
 public class Commit extends Command {
+    @CommandLine.Option(names = {"-m", "--message"}, required = true, description = "Use the given <msg> as the " +
+            "commit message. If multiple -m options are given, their values are concatenated as separate paragraphs.")
+    private String message;
+
     public void run() {
         // Run commit process
     }
