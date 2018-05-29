758dc15f66d80f38c2f87e2c6e41318dadb52dc8
dallanoe <enzo.dalla-nora@etu.unice.fr>
2018-05-29 15:33:52 +0200

    wrong junit import resulting in test not recognised, solved

--- src/test/java/fr/unice/polytech/pnsinnov/smartest/explorertree/plugins/strategytech/MvnExplorerTest.java
+++ src/test/java/fr/unice/polytech/pnsinnov/smartest/explorertree/plugins/strategytech/MvnExplorerTest.java
@@ -1,16 +1,16 @@
 package fr.unice.polytech.pnsinnov.smartest.explorertree.plugins.strategytech;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 import java.io.File;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-class MvnExplorerTest {
+public class MvnExplorerTest {
     @Test
-    void findAllModules() {
+    public void findAllModules() {
         MvnExplorer mvnExplorer = new MvnExplorer();
 
         assertEquals(1, mvnExplorer.findAllModules(new File(Paths.get("").toAbsolutePath().toString())).size());
