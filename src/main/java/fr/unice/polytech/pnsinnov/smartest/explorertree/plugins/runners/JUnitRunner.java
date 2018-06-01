340e134c96d53591936d167c3e0bad529399c064
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-06-01 14:05:31 +0200

    Fix scope class PNSE-28

--- src/main/java/fr/unice/polytech/pnsinnov/smartest/explorertree/plugins/runners/JUnitRunner.java
+++ src/main/java/fr/unice/polytech/pnsinnov/smartest/explorertree/plugins/runners/JUnitRunner.java
@@ -2,11 +2,10 @@ package fr.unice.polytech.pnsinnov.smartest.explorertree.plugins.runners;
 
 
 import fr.unice.polytech.pnsinnov.smartest.parser.Database;
-import org.junit.platform.engine.DiscoverySelector;
+import org.junit.platform.engine.discovery.ClassSelector;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.TestExecutionListener;
-import org.junit.platform.launcher.TestPlan;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
@@ -18,60 +17,50 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import static org.junit.platform.engine.discovery.DiscoverySelectors.*;
-
-public class JUnitRunner implements TRunner{
+public class JUnitRunner implements TRunner {
 
     @Override
     public boolean runAllTests(Set<String> toRun) {
         try {
-            File file = new File(Paths.get("target/test-classes/").toString());
-
-            URL[] testFolder = {file.toURI().toURL()};
+            File testPath = new File(Paths.get("target/test-classes/").toString());
+            File srcPath = new File(Paths.get("target/classes/").toString());
+            URL[] testFolder = {srcPath.toURI().toURL(), testPath.toURI().toURL()};
 
             URLClassLoader urlClassLoader = new URLClassLoader(testFolder);
 
             Set<Class> classes = new HashSet<>();
 
-            for (String test :
-                    toRun) {
+            for (String test : toRun) {
                 Set<String> itOnSet = Database.getInstance().getTestLinkToClass(test);
-                if (itOnSet != null){
-                    for (String singleTest :
-                            itOnSet) {
+                if (itOnSet != null) {
+                    for (String singleTest : itOnSet) {
                         Class c = urlClassLoader.loadClass(singleTest);
-
                         classes.add(c);
                     }
                 }
             }
 
-            for (Class cls: classes) {
-                System.out.println(cls);
-                Launcher launcher = LauncherFactory.create();
-
-                SummaryGeneratingListener listener = new SummaryGeneratingListener();
-                launcher.registerTestExecutionListeners(listener);
-                LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
-                        .selectors(selectClass(cls))
-                        .build();
-
-                launcher.discover(request);
-                launcher.execute(request);
-                listener.getSummary().getFailures().get(0).getException();
-                listener.getSummary().printTo(new PrintWriter(System.out));
-
-                if (listener.getSummary().getTestsFailedCount() != 0)
-                    return false;
-            }
-
-            return true;
-
-        } catch (MalformedURLException e) {
+            List<ClassSelector> selectors = classes.stream()
+                    .map(DiscoverySelectors::selectClass)
+                    .collect(Collectors.toList());
+            LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                    .selectors(selectors)
+                    .build();
+            Launcher launcher = LauncherFactory.create();
+            SummaryGeneratingListener listener = new SummaryGeneratingListener();
+            launcher.registerTestExecutionListeners(listener);
+            launcher.execute(request);
+            listener.getSummary().printTo(new PrintWriter(System.out));
+            return listener.getSummary().getTotalFailureCount() <= 0;
+        }
+        catch (MalformedURLException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e) {
             System.out.println("No class found for " + e.getLocalizedMessage());
         }
         return false;
