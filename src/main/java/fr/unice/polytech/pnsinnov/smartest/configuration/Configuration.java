577b068d6bb20cd6fb2f2658366548aa1a44ce0f
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-06-03 16:25:47 +0200

    Add configuration interfaces PNSE-30.

---
+++ src/main/java/fr/unice/polytech/pnsinnov/smartest/configuration/Configuration.java
@@ -0,0 +1,15 @@
+package fr.unice.polytech.pnsinnov.smartest.configuration;
+
+public interface Configuration {
+    String gitPath();
+
+    String projectPath();
+
+    String language();
+
+    String productionTool();
+
+    String testFramework();
+
+    String vcs();
+}
